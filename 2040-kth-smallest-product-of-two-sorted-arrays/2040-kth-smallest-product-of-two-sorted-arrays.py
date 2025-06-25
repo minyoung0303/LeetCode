687c060fe36c9c4ae6ba8fe79d59f347fa235c08
MinYoung <137133526+minyoung0303@users.noreply.github.com>
2025-06-25 23:12:58 +0900

    Time: 4829 ms (36.36%), Space: 17.9 MB (90.91%) - LeetHub

---
+++ 2040-kth-smallest-product-of-two-sorted-arrays/2040-kth-smallest-product-of-two-sorted-arrays.py
@@ -0,0 +1,42 @@
+import bisect,math
+
+class Solution(object):
+    def kthSmallestProduct(self, nums1, nums2, k):
+        """
+        We will take a value mid using binary search and 
+        try to see how many nums[i]*nums[j] values are lesser or equal to mid
+        if we get count>=k , we will look to the left part of binary search
+        else if count<=0 , we will look to the right part
+        """
+        def Check(mid):
+            n = len(nums1)
+            m = len(nums2)
+
+            count = 0
+            for n in nums1:
+                
+                # Binary search depending on the sign of number
+                if n > 0:
+                    count += bisect.bisect_right(nums2,mid//n)
+                elif n == 0:
+                    count += m if mid>=0 else 0
+                else:       
+                    v = mid//n
+                    if mid%n!=0:v+=1
+                    count += (m - bisect.bisect_left(nums2,v))
+          
+            return count>=k
+        
+    
+        l = -10**10
+        r = 10**10
+
+        while l<r:
+            mid = l + (r-l)//2
+
+            if Check(mid):
+                r = mid
+            else:
+                l = mid+1
+        
+        return r
