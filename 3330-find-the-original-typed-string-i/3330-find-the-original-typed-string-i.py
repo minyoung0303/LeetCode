0ff6bc5da3274b15157e49703ec6971b766b185b
MinYoung <137133526+minyoung0303@users.noreply.github.com>
2025-07-01 18:45:36 +0900

    Time: 12 ms (88.57%), Space: 12.5 MB (40%) - LeetHub

--- 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.py
+++ 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.py
@@ -1,23 +1,13 @@
 class Solution(object):
     def possibleStringCount(self, word):
-        """
-        :type word: str
-        :rtype: int
-        """
-        group_list = []
-        start = 0
-
+        res, cnt = 1, 1
         for i in range(1, len(word)):
-            if word[i] != word[i-1]:
-                group_list.append((word[i-1], i - start))
-                start = i
-
-        group_list.append((word[-1], len(word) - start))
-
-        result = 1
-        
-        for char, count in group_list:
-            if count >= 2:
-                result += count - 1
-
-        return result
+            if word[i] == word[i-1]:
+                cnt += 1
+            else:
+                if cnt >= 2:
+                    res += cnt - 1
+                cnt = 1
+        if cnt >= 2:
+            res += cnt - 1
+        return res
