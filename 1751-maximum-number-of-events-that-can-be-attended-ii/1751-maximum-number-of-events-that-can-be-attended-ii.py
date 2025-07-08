ae3433109197a4f39ed5405f850b8ed533c154b4
MinYoung <137133526+minyoung0303@users.noreply.github.com>
2025-07-08 22:46:43 +0900

    Time: 485 ms (100%), Space: 57.4 MB (88.89%) - LeetHub

---
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def maxValue(self, events, k):
+        events = sorted(events, key=lambda e: e[1])
+        n = len(events)
+        events_start_sorted = sorted([(e[0], i) for i, e in enumerate(events)])
+        
+        preceding = [-1] * n
+        j = 0
+        for start, index in events_start_sorted:
+            while j < n and events[j][1] < start:
+                j += 1
+            preceding[index] = j - 1
+
+        dp = [0] * n
+        res = 0
+        for j in range(1, k + 1):
+            max_value = -1
+            dp_next = [-1] * n
+            for i in range(n):
+                if j == 1:
+                    max_value = max(max_value, events[i][2])
+                elif preceding[i] >= 0 and dp[preceding[i]] >= 0:
+                    max_value = max(max_value, dp[preceding[i]] + events[i][2])
+                dp_next[i] = max_value
+            dp = dp_next
+            res = max(res, max_value)
+
+        return res       
